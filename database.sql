e5d9c6a6e6e0b6951dc5e8fb2e2d6a9ed9ab6a50
raff_run <rafa_pei@hotmail.com>
2018-11-21 16:39:59 -0200

    Inicial com 3 métodos rest

---
+++ database.sql
@@ -0,0 +1,248 @@
+-- Fabricantes
+CREATE TABLE public.tab_fabricantes
+(
+    pk_id_fabricante SERIAL,
+    nome character varying(255)  NOT NULL,
+    CONSTRAINT tab_fabricantes_pkey PRIMARY KEY (pk_id_fabricante)
+);
+
+-- Modelos
+CREATE TABLE public.tab_modelos
+(
+    pk_id_modelo SERIAL,
+    nome character varying(255)  NOT NULL,
+    fk_fabricante integer,
+    CONSTRAINT tab_modelos_pkey PRIMARY KEY (pk_id_modelo),
+    FOREIGN KEY (fk_fabricante)
+        REFERENCES public.tab_fabricantes (pk_id_fabricante)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
+
+-- Carros
+CREATE TABLE public.tab_carros
+(
+    pk_id_carro SERIAL,
+    ano integer NOT NULL,
+    detalhe character varying(255)  NOT NULL,
+    fk_fabricante integer,
+    fk_modelo integer,
+    CONSTRAINT tab_carros_pkey PRIMARY KEY (pk_id_carro),
+    FOREIGN KEY (fk_modelo)
+        REFERENCES public.tab_modelos (pk_id_modelo)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    FOREIGN KEY (fk_fabricante)
+        REFERENCES public.tab_fabricantes (pk_id_fabricante)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
+
+--Usuários
+CREATE TABLE public.tab_usuarios
+(
+    pk_id_usuario SERIAL,
+    cidade character varying(255)  NOT NULL,
+    email character varying(255)  NOT NULL,
+    login character varying(30)  UNIQUE NOT NULL,
+    nome character varying(255)  NOT NULL,
+    senha character varying(20)  NOT NULL,
+    uf character varying(2)  NOT NULL,
+    CONSTRAINT tab_usuarios_pkey PRIMARY KEY (pk_id_usuario)
+);
+
+-- Carros de usuários
+
+CREATE TABLE public.tab_carros_usuario
+(
+    pk_id_carro_usuario SERIAL,
+    anofabricacao integer,
+    chassi character varying(20)  UNIQUE NOT NULL,
+    data_compra date,
+    quilometragem integer,
+    fk_id_carro integer,
+    fk_id_usuario integer,
+    CONSTRAINT tab_carros_usuario_pkey PRIMARY KEY (pk_id_carro_usuario),
+    FOREIGN KEY (fk_id_carro)
+        REFERENCES public.tab_carros (pk_id_carro)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    FOREIGN KEY (fk_id_usuario)
+        REFERENCES public.tab_usuarios (pk_id_usuario)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
+
+--Histórico
+CREATE TABLE public.tab_historicos
+(
+    pk_id_historico SERIAL,
+    dadosobd jsonb NOT NULL,
+    latitudechegada integer,
+    latitudepartida integer,
+    longitudechegada integer,
+    longitudepartida integer,
+    dataHoraEnvio timestamp default now(),
+    fk_id_carrousuario integer,
+    CONSTRAINT tab_historicos_pkey PRIMARY KEY (pk_id_historico),
+    FOREIGN KEY (fk_id_carrousuario)
+        REFERENCES public.tab_carros_usuario (pk_id_carro_usuario) 
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
+
+
+-- Endereços
+CREATE TABLE public.tab_enderecos
+(
+    pk_id_endereco SERIAL,
+    bairro character varying(255) NOT NULL,
+    cep integer NOT NULL,
+    cidade character varying(255) NOT NULL,
+    complemento character varying(255) NOT NULL,
+    latitude integer NOT NULL,
+    logradouro character varying(255) NOT NULL,
+    longitude integer NOT NULL,
+    numero character varying(255) NOT NULL,
+    referencia character varying(255) NOT NULL,
+    uf character varying(2) NOT NULL,
+    CONSTRAINT tab_enderecos_pkey PRIMARY KEY (pk_id_endereco)
+);
+
+-- Lojas
+
+CREATE TABLE public.tab_lojas
+(
+    pk_id_loja SERIAL,
+    cnpj character varying(255)  NOT NULL,
+    nome character varying(255)  NOT NULL,
+    fk_endereco integer,
+    fk_fabricante integer,
+    imagem oid,
+    CONSTRAINT tab_lojas_pkey PRIMARY KEY (pk_id_loja),
+    FOREIGN KEY (fk_endereco)
+        REFERENCES public.tab_enderecos (pk_id_endereco) 
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    FOREIGN KEY (fk_fabricante)
+        REFERENCES public.tab_fabricantes (pk_id_fabricante) 
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
+
+-- Serviços
+
+CREATE TABLE public.tab_servicos
+(
+    pk_id_servico SERIAL,
+    nome character varying(255)  NOT NULL,
+    CONSTRAINT tab_servicos_pkey PRIMARY KEY (pk_id_servico)
+);
+
+CREATE TABLE public.tab_presta_servicos
+(
+    pk_id_presta_servico SERIAL,
+    preco real,
+    fk_id_loja integer,
+    fk_id_servico integer,
+    CONSTRAINT tab_presta_servicos_pkey PRIMARY KEY (pk_id_presta_servico),
+    FOREIGN KEY (fk_id_loja)
+        REFERENCES public.tab_lojas (pk_id_loja) 
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    FOREIGN KEY (fk_id_servico)
+        REFERENCES public.tab_servicos (pk_id_servico) 
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
+
+-- Sessões
+CREATE TABLE public.tab_sessoes
+(
+    pk_id_sessao SERIAL,
+    token character varying(255)  UNIQUE NOT NULL,
+    validoate timestamp without time zone NOT NULL,
+    fk_id_usuario integer,
+    CONSTRAINT tab_sessoes_pkey PRIMARY KEY (pk_id_sessao),
+    FOREIGN KEY (fk_id_usuario)
+        REFERENCES public.tab_usuarios (pk_id_usuario) 
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
+
+-- Functions para retornar JSON
+
+-- Busca de forma sucinta por lojas usando parte dos seus nomes. Para ser usado na barra de pesquisa.
+create or replace function buscarLojasPorNome(varchar(255))  
+returns setof json
+as $$
+DECLARE
+	nomeBuscaLike varchar := ('%'|| $1 || '%');
+begin 
+		
+  return query select json_agg(row_to_json(t) order by length(nome) asc) as lojas
+					from (
+					  select l.pk_id_loja, l.nome
+					  from tab_lojas l
+						where l.nome ILIKE nomeBuscaLike
+						LIMIT 15
+					) t;
+end;
+$$ 
+language plpgsql;
+
+-- retorna uma "página" de históricos de um carro em um determinado período.
+create or replace function lerQuantHistoricoJson(integer, timestamp, timestamp, integer, integer)  
+returns setof json
+as $$
+DECLARE
+data_inicio timestamp;
+data_fim timestamp;
+begin 
+	if($3 is null) then
+		data_fim := now();
+	else
+		data_fim := $3;
+	end if;
+	if($2 is null) then
+		data_inicio := '01/01/1500 00:00:00.000000';
+	else
+		data_inicio := $2;
+	end if;
+	
+		
+  return query select json_agg(row_to_json(t)) as lojas
+					from (
+					  select h.pk_id_historico, h.dadosobd, h.latitudepartida, h.longitudepartida, h.latitudechegada, h.longitudechegada, h.dataHoraEnvio, car.fk_id_usuario
+					  from tab_historicos h join tab_carros_usuario car ON (h.fk_id_carrousuario = car.pk_id_carro_usuario)
+						where h.dataHoraEnvio >= data_inicio and h.dataHoraEnvio <= data_fim and h.fk_id_carrousuario = $1
+						LIMIT $4 OFFSET $5
+					) t;
+end;
+$$ 
+language plpgsql;
+
+-- Retorna uma "página" de lojas com todas as informações das lojas, seus serviços e preços
+create or replace function lerQuantLojasCompletaJson(integer, integer)  
+returns setof json
+as $$
+begin 
+  return query select json_agg(row_to_json(t)) as lojas
+					from (
+					  select l1.pk_id_loja, l1.nome, l1.cnpj, f.nome as autorizada,
+						(
+						  select array_to_json(array_agg(row_to_json(d)))
+						  from (
+							select s.nome, ps.preco
+							from tab_servicos s
+							  join tab_presta_servicos ps ON (s.pk_id_servico = ps.fk_id_servico)
+							  join tab_lojas l ON (l.pk_id_loja = ps.fk_id_loja)
+							  where l.pk_id_loja = l1.pk_id_loja
+							order by nome asc
+						  ) d
+						) as servicos
+					  from tab_lojas l1 LEFT OUTER JOIN tab_fabricantes f ON (l1.fk_fabricante = f.pk_id_fabricante) LIMIT $1 OFFSET $2
+					) t;
+end;
+$$ 
+language plpgsql;
---
+++ routes/queries.js
@@ -0,0 +1,141 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var db = pgp({
+  host: 'localhost',
+  port: 5432,
+  database: 'pecapreco',
+  user: 'postgres',
+  password: 'postgres'
+});
+
+// add query functions
+
+module.exports = {
+  getPaginaLojasCompletas: getPaginaLojasCompletas,
+  getPaginaHistoricos: getPaginaHistoricos,
+  getLojasMinimasPorNome: getLojasMinimasPorNome//,
+  //getLojaCompleta: getLojaCompleta,
+  //getCarroUsuario: getCarroUsuario,
+  //postCriarUsuario: postCriarUsuario,
+  //postCriarSessao: postCriarSessao,
+  //postCriarHistorico: postCriarSessao,
+  //postCriarCarroUsuario: postCriarCarroUsuario,
+  //postUploadHistorico: postUploadHistorico
+};
+
+// Pega uma página de lojas ao dar a quantidade de lojas em uma "página" e a sua posição na "página"
+function getPaginaLojasCompletas(req, res, next) {
+  var quantLojas = parseInt(req.query.quant);
+  var lojaIndex = parseInt(req.query.index);
+
+  if(isNaN(quantLojas) || isNaN(lojaIndex)){
+    res.status(400);
+    res.send('Parâmetros inválidos.');
+    return;
+  }
+  
+  db.one('select lerQuantLojasCompletaJson($1,$2) as lojas', [quantLojas, lojaIndex])
+    .then(function (data) {
+      res.status(200).send(data);
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  
+}
+
+// Pega uma lista de nomes e ids de lojas ao ser fornecido parte de um nome
+function getLojasMinimasPorNome(req, res, next) {
+  let regex = new RegExp('^[a-zA-Z]+( *\\p{L}*)*$/u'); // prevenção de injection
+  var parteNome = req.query.nome;
+  parteNome = parteNome.trim();
+  console.log(parteNome);
+  if(typeof parteNome == 'undefined' || !regex.test(parteNome) || parteNome == ""){
+    res.status(400);
+    res.send('Parâmetros inválidos.');
+    return;
+  }
+  
+  db.one('select * from buscarLojasPorNome($1) as resultados', [parteNome])
+    .then(function (data) {
+      res.status(200).send(data);
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  
+}
+
+// Pega uma página de historicos ao dar o carroUsuario relacionado, a timestamp de inicio e fim da busca e a quantidade de resultados + posição na página
+function getPaginaHistoricos(req, res, next) {
+  var tokenSessao = req.query.tokenSessao;
+  var idCarroUsuario = parseInt(req.query.id);
+  var diaInicio = parseInt(req.query.diaInicio);
+  var mesInicio = parseInt(req.query.mesInicio);
+  var anoInicio = parseInt(req.query.anoInicio);
+  var horaInicio = parseInt(req.query.horaInicio);
+  var minutoInicio = parseInt(req.query.minutoInicio);
+  var segundoInicio = parseInt(req.query.segundoInicio);
+  var diaFim = parseInt(req.query.diaFim);
+  var mesFim = parseInt(req.query.mesFim);
+  var anoFim = parseInt(req.query.anoFim);
+  var horaFim = parseInt(req.query.horaFim);
+  var minutoFim = parseInt(req.query.minutoFim);
+  var segundoFim = parseInt(req.query.segundoFim);
+  var quantHistorico = parseInt(req.query.quant);
+  var historicoIndex = parseInt(req.query.index);
+
+  var dataHoraInicio;
+  var dataHoraFim;
+
+  if(isNaN(diaInicio) && isNaN(mesInicio) && isNaN(anoInicio) && isNaN(horaInicio) && isNaN(minutoInicio) && isNaN(segundoInicio)){
+    dataHoraInicio = null;
+    //Pra passar pelo if gigante
+    diaInicio = 00;
+    mesInicio = 00;
+    anoInicio = 00;
+    horaInicio = 00;
+    minutoInicio = 00;
+    segundoInicio = 00;
+  }
+  if(isNaN(diaFim) && isNaN(mesFim) && isNaN(anoFim) && isNaN(horaFim) && isNaN(minutoFim) && isNaN(segundoFim)){
+    dataHoraFim = null;
+    //Pra passar pelo if gigante
+    diaFim = 00;
+    mesFim = 00;
+    anoFim = 00;
+    horaFim = 00;
+    minutoFim = 00;
+    segundoFim = 00;
+  }
+
+  if(isNaN(quantHistorico) || isNaN(historicoIndex) || isNaN(idCarroUsuario) 
+    || isNaN(diaInicio) || isNaN(mesInicio) || isNaN(anoInicio) || isNaN(horaInicio) || isNaN(minutoInicio) || isNaN(segundoInicio)
+    || isNaN(diaFim) || isNaN(mesFim) || isNaN(anoFim) || isNaN(horaFim) || isNaN(minutoFim) || isNaN(segundoFim)){
+    res.status(400);
+    res.send('Parâmetros inválidos.');
+    return;
+  }
+
+  if(typeof dataHoraInicio == 'undefined'){
+    dataHoraInicio = new Date(anoInicio, mesInicio, diaInicio, horaInicio, minutoInicio, segundoInicio);
+  }
+  if(typeof dataHoraFim == 'undefined'){
+    dataHoraFim = new Date(anoFim, mesFim, diaFim, horaFim, minutoFim, segundoInicio);
+  }
+  
+  db.one('select lerQuantHistoricoJson($1, $2, $3, $4, $5) as historicos', [idCarroUsuario, dataHoraInicio, dataHoraFim, quantHistorico, historicoIndex])
+    .then(function (data) {
+      res.status(200).send(data);
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  
+}
